6fbd5833beed1f64b0672bcbd6bcf5c81f256ad4
jaki <654641423@qq.com>
2023-10-26 08:53:36 +0800

    a

--- app/src/main/java/com/example/jitpacktest/MainActivity.kt
+++ app/src/main/java/com/example/jitpacktest/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.jitpacktest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.testlib.Test
-import com.example.testlib2.Test2
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Test().test()
-        Test2().test2()
     }
 }
